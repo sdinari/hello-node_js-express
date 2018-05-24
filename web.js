fddc9a448fc784d3146261761d6e1f08116730bb
S. D <dinarigem@yahoo.fr>
2018-05-24 00:14:05 +0000

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello', 'Sdinari123');
+  res.send('hello said');
 });
 
 app.listen(process.env.PORT || 3000);
